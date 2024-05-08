6c5e400aa9041bb9ccba70ed50e1afcff1651aab
Ramanujam Gond <ramanujamgond@gmail.com>
2024-05-09 01:03:31 +0530

    feat: develop the home screen for the coffee app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
+import TabNavigator from './src/navigators/TabNavigator';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 
@@ -11,9 +11,20 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='Details' component={DetailsScreen} options={{ animation: 'slide_from_bottom' }}>
+        <Stack.Screen
+          name='tabs'
+          component={TabNavigator}
+          options={{ animation: 'slide_from_bottom' }}>
         </Stack.Screen>
-        <Stack.Screen name='Paymnet' component={PaymentScreen} options={{ animation: 'slide_from_bottom' }}>
+        <Stack.Screen
+          name='Details'
+          component={DetailsScreen}
+          options={{ animation: 'slide_from_bottom' }}>
+        </Stack.Screen>
+        <Stack.Screen
+          name='Paymnet'
+          component={PaymentScreen}
+          options={{ animation: 'slide_from_bottom' }}>
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
